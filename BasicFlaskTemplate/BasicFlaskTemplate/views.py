1d36153961962ea735bbe11c43ba6c91d2d4a678
MCG <martygleeson@Hotmail.com>
2022-05-13 11:09:22 +1000

    yes
    yes

---
+++ BasicFlaskTemplate/BasicFlaskTemplate/views.py
@@ -0,0 +1,48 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from BasicFlaskTemplate import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/test')
+def test():
+    """Renders the about page."""
+    return render_template(
+        'test.html',
+        title='Test',
+        year=datetime.now().year,
+        message='Test page.'
+    )
+
---
+++ BasicFlaskTemplate/runserver.py
@@ -0,0 +1,15 @@
+"""
+This script runs the BasicFlaskTemplate application using a development server.
+"""
+
+from os import environ
+from BasicFlaskTemplate import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5555
+        print("5555")
+    app.run(HOST, PORT)
