3f715c6824c6c43bca5784fce7dd1eec87c53471
MCG <martygleeson@Hotmail.com>
2022-05-17 23:27:47 +1000

    Proof of concept
    Updated Dockerfile
    Updated Flask application
     --Intergrated automation of processing graphs from image
    --Note: results not consistant

--- BasicFlaskTemplate/BasicFlaskTemplate/upload.py
+++ BasicFlaskTemplate/BasicFlaskTemplate/upload.py
@@ -9,7 +9,8 @@ from re import A
 from flask import Flask, render_template,redirect, request, url_for, send_from_directory
 from BasicFlaskTemplate import app
 from werkzeug.utils import secure_filename
-
+from BasicFlaskTemplate import ImageColorProcessor as ICP
+import matplotlib.pyplot as plt
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
@@ -28,7 +29,18 @@ def upload_image():
 		print("Fname:"+fname)
 		basedir = os.path.abspath(os.path.dirname(__file__))
 		file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], fname))
-		return render_template("test.html", provided_image=os.path.join(app.config['UPLOAD_FOLDER'], fname))
+
+		#process the image and display graph
+		plt = ICP.start(os.path.join(basedir, app.config['UPLOAD_FOLDER'], fname), 10)
+		
+		#producedGraph = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+		export_name = "graph_"+fname
+		plt.savefig(os.path.join(basedir, app.config['UPLOAD_FOLDER'], export_name))
+		plt.close()
+		#returns to the test page with data.. should make this index?
+		return render_template("test.html", generated_graph=os.path.join(app.config['UPLOAD_FOLDER'], export_name), provided_image=os.path.join(app.config['UPLOAD_FOLDER'], fname))
+		#return render_template("test.html", generated_graph=os.path.join(app.config['UPLOAD_FOLDER'], export_name))
+		#return render_template("test.html", basedir + "export.png")
 	else:
 		return redirect(request.url)
 
